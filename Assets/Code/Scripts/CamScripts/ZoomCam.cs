57608aba3b4c315df11020a69b92cef74ff4f759
oldfashionedrobot <sanjeev@oldfashionedrobot.com>
2014-01-04 14:58:22 -0500

    added blurring to zoom functionality

---
+++ Assets/Code/Scripts/CamScripts/ZoomCam.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class ZoomCam : MonoBehaviour {
+  
+	public void Update() {
+    if( Input.GetMouseButton( 0 ) ) {
+      GameObject currentZoomView = GameObject.FindGameObjectsWithTag( "ZoomView" )[0];
+      currentZoomView.SetActive( false );
+      Blur camBlur = Camera.main.GetComponent( "Blur" ) as Blur;
+      camBlur.enabled = false;
+      gameObject.SetActive( false );
+    }
+  }
+}
--- Assets/Code/Scripts/CamScripts/ZoomClicker.cs
+++ Assets/Code/Scripts/CamScripts/ZoomClicker.cs
@@ -4,19 +4,24 @@ using System.Collections;
 public class ZoomClicker : MonoBehaviour {
 
   private Texture2D cursor;
+  private Blur camBlur;
+  private GameObject zoomCam;
   
   public GameObject zoomView;
 
   public void Start() {
     cursor = Resources.Load( "Cursors/cursor_eye" ) as Texture2D;
     zoomView.SetActive( false );
+    camBlur = Camera.main.GetComponent( "Blur" ) as Blur;
+    zoomCam = Camera.main.transform.Find( "ZoomCam" ).gameObject;
   }
   
-	public void OnMouseDown() {
+	public void OnMouseUp() {
     Vector3 camPos = Camera.main.transform.position;
     zoomView.SetActive( true );
     zoomView.transform.position = new Vector3( camPos.x, camPos.y, -9 );
-    GameObject.Instantiate( Resources.Load( "Prefabs/ZoomOverlay" ) );
+    camBlur.enabled = true;
+    zoomCam.SetActive( true );
   }
   
   public void OnMouseOver() {
@@ -24,6 +29,6 @@ public class ZoomClicker : MonoBehaviour {
   }
   
   public void OnMouseExit() {
-  		Cursor.SetCursor( null, Vector2.zero, CursorMode.Auto );
+		Cursor.SetCursor( null, Vector2.zero, CursorMode.Auto );
 	}
 }
