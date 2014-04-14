4e8ded1dc88fa6900162a06d602e514598ce4097
oldfashionedrobot <sanjeev@oldfashionedrobot.com>
2014-04-14 13:27:00 -0400

    folder organization

---
+++ Assets/Code/Level_1/Things/FlushHandle.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class FlushHandle : MonoBehaviour {
+  
+  private Sprite cursor;
+  
+  public bool isPowered = false;
+  
+  void Start() {
+    cursor = Resources.Load<Sprite>( "Cursors/cursor_hand" );
+  }
+  
+  void OnClick() {
+    if( !isPowered ) {
+      Debug.Log( "normal flush" );
+    } else {
+      Debug.Log( "suck in pig" );
+      Destroy( GameObject.Find( "Pig" ) );
+      Game.cookies.Add( "pigInSewer", true );
+    }
+  }
+
+  
+  void OnHover( bool isOver ) {
+    Game.CursorHover( isOver, cursor );
+  }
+}
