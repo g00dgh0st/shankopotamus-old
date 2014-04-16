de169bdaded6e2ae8856f9a107f1e7a3078182e4
oldfashionedrobot <sanjeev@oldfashionedrobot.com>
2014-04-15 23:36:51 -0400

    pig flushing action

---
+++ Assets/Code/Level_1/Things/PigSewer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PigSewer : MonoBehaviour {
+
+  private bool moving = false;
+  private bool fadeStarted = false;
+  private bool bubShown = false;
+  
+  void Start() {
+    Game.cookies.Add( "pigInSewer", true );
+  }
+
+  void OnEnable() {
+    if( Game.cookies != null && Game.cookies.Contains( "pigInSewer" ) ) {
+      StartCoroutine( Engage() );
+    }
+  }
+  
+  private IEnumerator Engage() {
+    while( Game.clicksPaused || Game.player.InMotion() ) {
+      yield return null;
+    } 
+    
+    Game.PauseClicks();
+    Game.PauseCam();
+    moving = true;
+    Game.cookies.Remove( "pigInSewer" );
+    transform.Find( "Sprites" ).gameObject.SetActive( true );
+  }
+  
+  void Update() {
+    if( moving ) {
+      transform.position = new Vector3( transform.position.x + 0.04f, transform.position.y, transform.position.z );
+      
+      Vector3 pos = Camera.main.transform.position;
+      Vector3 newPos;
+      Vector3 diff = new Vector3( transform.position.x + 1f, transform.position.y + 0.5f, -20f ) - pos;
+      newPos = pos + ( diff * 0.05f );
+      
+      Camera.main.transform.position = newPos;
+
+      if( transform.localPosition.x > 3.2f && !fadeStarted ) {
+        fadeStarted = true;
+        StartCoroutine( Game.FadeCamera( delegate() {
+          GameObject.Find( "Rooms" ).transform.Find( "SewersRight" ).gameObject.SetActive( true );
+          transform.localPosition = new Vector3( 5.5f, transform.localPosition.y, transform.localPosition.z );
+          Camera.main.transform.position = new Vector3( transform.position.x + 1f, transform.position.y + 0.5f, -20f );
+        } ) );
+      }
+      
+      if( !bubShown && transform.position.x > Game.player.transform.position.x ) {
+        Game.script.ShowSpeechBubble( "Not the best way to die.", transform.Find( "BubTarget" ), 5f );
+        bubShown = true;
+      }
+    }
+    
+  }
+}
--- Assets/Code/Level_1/Things/Whale.cs
+++ Assets/Code/Level_1/Things/Whale.cs
@@ -14,21 +14,35 @@ public class Whale : MonoBehaviour {
     cursor = Resources.Load<Sprite>( "Cursors/cursor_hand" );
   }
   
-  void MoveWhale() {
+  IEnumerator MoveWhale() {
     waypoint1.SetActive( true );
     waypoint2.SetActive( true );
     Debug.Log( "Knock Whale" );
-    Destroy( gameObject );
+    gameObject.GetComponent<SpriteRenderer>().enabled = false;
+    
+    yield return new WaitForSeconds( 1.5f );
+    
+    StartCoroutine( Game.FadeCamera( delegate() {
+      Game.ResumeClicks();
+      Game.ResumeCam();
+      
+      Vector3 plPos = Game.player.transform.Find( "CamTarget" ).position;
+      Camera.main.transform.position = new Vector3( plPos.x, plPos.y, -20f );
+      
+      GameObject.Find( "SewersRight" ).SetActive( false );
+      
+      Destroy( gameObject );
+    } ) );
   }
   
   void OnClick() {
     Game.player.MoveTo( movepoint.position, delegate() { Game.script.ShowSpeechBubble( "This whale is blocking the path. \nMaybe if I could flush something really big into the sewers...", Game.player.transform.Find( "BubTarget" ), 5f ); } );
   }
   
-  void OnTriggerEnter( Collider collider ) {
-    if( collider.gameObject.name == "Pig" ) {
+  void OnCollisionEnter2D( Collision2D collider ) {
+    if( collider.gameObject.name == "PigSewer" ) {
       Destroy( collider.gameObject );
-      MoveWhale();
+      StartCoroutine( MoveWhale() );
     }
   }
   
