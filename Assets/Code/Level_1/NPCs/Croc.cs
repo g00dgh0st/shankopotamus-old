1459427542774e1f884d5952cdcc9599061fed48
oldfashionedrobot <sanjeev@oldfashionedrobot.com>
2014-08-18 14:47:09 -0400

    Croc rewrite

--- Assets/Code/Level_1/NPCs/Croc.cs
+++ Assets/Code/Level_1/NPCs/Croc.cs
@@ -10,6 +10,7 @@ public class Croc : MonoBehaviour {
   public Bear bear;
   
   public bool wantsGlasses = false;
+  public bool knowShank = false;
   
   void Start() {
     cursor = Resources.Load<Sprite>( "Cursors/cursor_chat" );
@@ -33,7 +34,7 @@ public class Croc : MonoBehaviour {
 
   void OnClick() {
     if( wantsGlasses )
-      Game.player.MoveTo( transform.position, delegate( bool b ) { Game.dialogueManager.StartDialogue( dialogue, 10 ); } );
+      Game.player.MoveTo( transform.position, delegate( bool b ) { Game.dialogueManager.StartDialogue( dialogue, 15 ); } );
     else
       Game.player.MoveTo( transform.position, delegate( bool b ) { Game.dialogueManager.StartDialogue( dialogue, 0 ); } );
   }
@@ -53,79 +54,116 @@ public class Croc : MonoBehaviour {
       // 0
       new Step( camTarget, "Hello.",
         new Option[] {
-          new Option( "I heard you're the toughest guy in the prison.", 1 ),
-          new Option( "You have a lot of books here.", 6 ),
-          new Option( "I heard you have some wine?", 7, delegate() { return bear.wantsWine && GameObject.Find( "item_wine_bottle" ) == null && GameObject.Find( "item_empty_bottle" ) == null; } ),
+          new Option( "I heard you make wine.", 10, delegate() { return bear.wantsWine && GameObject.Find( "item_wine_bottle" ) == null && GameObject.Find( "item_empty_bottle" ) == null; } ),
+          new Option( "Do you know how I can get a shank in here?", 6, delegate() { return knowShank; } ),
+          new Option( "A little bird told me you're the toughest guy in the prison.", 1, delegate() { return !knowShank; }),
+          new Option( "A little bird told me you're the toughest guy in the prison.", 3, delegate() { return knowShank; }),
+          new Option( "What's with all the books?", 7 ),
           new Option( "Bye.", -1 )
         }
       ),
       // 1
-      new Step( camTarget, "That may be true. I have allegedly shanked many a chump.",
+      new Step( camTarget, "Did Steve tell you that? I'll have to make a tiny shank for him.", 
         new Option[] {
-          new Option( "Why have you allegedly shanked so many people?", 2 ),
-          new Option( "You have a lot of books here.", 6 ), 
-          new Option( "I heard you have some wine?", 7, delegate() { return bear.wantsWine && GameObject.Find( "item_wine_bottle" ) == null && GameObject.Find( "item_empty_bottle" ) == null; } )
-        } 
-      ),
-      // 2 
-      new Step( camTarget, "For various reasons, allegedly. Sometimes for money, sometimes in anger. And sometimes just for fun.",
-        new Option[] {
-          new Option( "So you're a hitman? Allegedly?", 3 ),
-          new Option( "You have a lot of books here.", 6 ),
-          new Option( "I heard you have some wine?", 7, delegate() { return bear.wantsWine && GameObject.Find( "item_wine_bottle" ) == null && GameObject.Find( "item_empty_bottle" ) == null; } )
+          new Option( "Steve?", 2 ),
+          new Option( "I heard that you shanked a lot of people.", 3 )
         }
       ),
+      // 2
+      new Step( camTarget, "He's a bird that talks too much. But yes, many people say that I'm the toughest guy in this prison.", 3 ),
       // 3
-      new Step( camTarget, "One might say that. Then again, one might say that I just enjoy shanking. But that one is probably dead. Allegedly.",
+      new Step( camTarget, "I have allegedly shanked many a chump.", 
         new Option[] {
-          new Option( "Do you have any tips for shanking someone?", 4 ),
-          new Option( "You have a lot of books here.", 6 ),
-          new Option( "I heard you have some wine?", 7, delegate() { return bear.wantsWine && GameObject.Find( "item_wine_bottle" ) == null && GameObject.Find( "item_empty_bottle" ) == null; } )
-        }
+          new Option( "Why have you allegedly shanked so many people?", 4 ),
+          new Option( "So you're a hitman? Allegedly?", 5 ),
+          new Option( "Do you know how I can get a shank in here?", 6 )
+        },
+        delegate() { knowShank = true; }  
       ),
       // 4
-      new Step( camTarget, "Well, I do allegedly have a lot of experience. I would recommend looking around the cafeteria. There's plenty of material there to fashion a solid shank.", 5 ),
-      // 5
-      new Step( camTarget, "And remember to stretch before the act. You wouldn't want to pull a hammy.",
+      new Step( camTarget, "For various reasons, allegedly. Sometimes for money, sometimes in anger. And sometimes just for fun.", 
         new Option[] {
-          new Option( "Thanks for the advice. I may allegedly use it sometime.", -1 ),
-          new Option( "You have a lot of books here.", 6 ),
-          new Option( "I heard you have some wine?", 7, delegate() { return bear.wantsWine && GameObject.Find( "item_wine_bottle" ) == null && GameObject.Find( "item_empty_bottle" ) == null; } )
+          new Option( "So you're a hitman? Allegedly?", 5 ),
+          new Option( "Do you know how I can get a shank in here?", 6 )
+        }
+      ),
+      // 5
+      new Step( camTarget, "One might say that. Then again, one might say that I just enjoy shanking. But that one is probably dead. Allegedly.", 
+        new Option[] {
+          new Option( "Do you know how I can get a shank in here?", 6 ),
+          new Option( "Let me ask you something else.", 0 ),
+          new Option( "I'll see you later", -1 )
         }
       ),
       // 6
-      new Step( camTarget, "I enjoy reading. It keeps my mind as sharp as my shanks. Alleged shanks.",
+      new Step( camTarget, "You can make a shank out of anything, allegedly. The only limit is your imagination.", 
         new Option[] {
-          new Option( "I heard you're the toughest guy in the prison", 1 ),
-          new Option( "Do you have any wine?", 7, delegate() { return bear.wantsWine && GameObject.Find( "item_wine_bottle" ) == null && GameObject.Find( "item_empty_bottle" ) == null; } ),
-          new Option( "Bye.", -1 )
+          new Option( "Let me ask you something else.", 0 ),
+          new Option( "I'll see you later", -1 )
         }
       ),
       // 7
-      new Step( camTarget, "Yes, I make a fine cabernet in my toilet. Would you like a taste?",
+      new Step( camTarget, "I enjoy reading. It keeps my mind as sharp as my shanks. Alleged shanks.", 
         new Option[] {
-          new Option( "Could I get a bottle of it?", 8 )
+          new Option( "Have you read all of these?", 8 ),
+          new Option( "Let me ask you something else.", 0 ),
+          new Option( "I'll see you later", -1 )
         }
       ),
-      // 8
-      new Step( camTarget, "Well that will come at a price. My eyes unfortunately aren't what they used to be, and it makes reading all the books quite difficult. If you could go find me some glasses, then I would be happy to part with a bottle of wine.",
+      // 8 
+      new Step( camTarget, "Why yes. Many times. I can recite most of these from memory.", 
         new Option[] {
-          new Option( "Any idea where I can find some glasses?", 9 )
-        },
-        delegate() { wantsGlasses = true; }
+          new Option( "What's your favorite?", 9 ),
+          new Option( "Let me ask you something else.", 0 ),
+          new Option( "I'll see you later", -1 )
+        }
       ),
       // 9
-      new Step( camTarget, "One of the other inmates must have a pair, you should ask around.",
+      new Step( camTarget, "My favorite is \"Please Stop, Amelia Bedelia\". Amelia snaps and murders the Rogers. But she does so in a hilariously clumsy way.", 
         new Option[] {
-          new Option( "I'll go do that.", -1 )
+          new Option( "Let me ask you something else.", 0 ),
+          new Option( "I'll see you later", -1 )
         }
       ),
       // 10
+      new Step( camTarget, "Yes I make a fine cabernet in my toilet. Would you like a taste?", 
+        new Option[] {
+          new Option( "How do you make it?", 11 ),
+          new Option( "Can I get a bottle of it?", 13 )
+        }
+      ),
+      // 11
+      new Step( camTarget, "I painstakingly source only the finest, best quality grape juice concentrate from the prison commissary. ", 12 ), 
+      // 12
+      new Step( camTarget, "And then I dump it into my toilet with sugar and yeast.", 
+        new Option[] {
+          new Option( "Classy. Can I get a bottle of it?", 13 )
+        }
+      ),
+      // 13
+      new Step( camTarget, "You may trade me for it. My eyes unfortunately aren't what they used to be, and it makes reading quite difficult. If you could go find me some glasses, then I would be happy to part with a bottle of wine.",
+        new Option[] {
+          new Option( "Any idea where I can find some glasses?", 14 ),
+          new Option( "I'll go look for some glasses.", -1 ),
+          new Option( "Let me ask you something else.", 0 )
+        },
+        delegate() { wantsGlasses = true; }
+      ),
+      // 14
+      new Step( camTarget, "One of the inmates must have a pair. Ask around.", 
+        new Option[] {
+          new Option( "I'll go look for some glasses.", -1 ),
+          new Option( "Let me ask you something else.", 0 )
+        }
+      ),
+      // 15
       new Step( camTarget, "Were you able to find some glasses?",
         new Option[] {
-          new Option( "I'll go do that.", -1 ),
-          new Option( "I heard you're the toughest guy in the prison.", 1 ),
-          new Option( "You have a lot of books here.", 6 )
+          new Option( "No, I'll go look for them.", -1 ),
+          new Option( "Do you know how I can get a shank in here?", 6, delegate() { return knowShank; } ),
+          new Option( "A little bird told me you're the toughest guy in the prison.", 1, delegate() { return !knowShank; }),
+          new Option( "A little bird told me you're the toughest guy in the prison.", 3, delegate() { return knowShank; }),
+          new Option( "What's with all the books?", 7 )
         }
       )
     } );
