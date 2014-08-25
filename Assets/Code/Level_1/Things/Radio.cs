9bf4272e8be05bfefbb169e52f0a876b5454a536
oldfashionedrobot <sanjeev@oldfashionedrobot.com>
2014-08-25 14:35:25 -0400

    radio and shower guy placeholdesr

--- Assets/Code/Level_1/Things/Radio.cs
+++ Assets/Code/Level_1/Things/Radio.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class Radio : MonoBehaviour {
+public class Radio : Clicker {
   
   private Sprite cursor;
   
@@ -10,17 +10,33 @@ public class Radio : MonoBehaviour {
   }
   
   void OnClick() {
-    Game.player.MoveTo( transform.position, delegate( bool b ) { 
-      GuardTowerGuard scrip = Game.GetScript<GuardTowerGuard>();
-    
-      if( !scrip.atScreens && scrip.firstTime ) {
-        Game.GetScript<GuardTowerGuard>().OnClick();
-      } else if( !scrip.atScreens && !scrip.firstTime ) {
-        scrip.DontTouchBubble();
-      } else if( scrip.atScreens ) {
-        Debug.Log( "Break, get battery" );
-        Game.script.AddItem( "battery" );
-        Destroy( gameObject );
+    Game.player.MoveTo( movePoint, delegate( bool b ) { 
+      Game.player.FaceTarget( transform.position );
+      
+      if( Game.cookies.Contains( "showersOn" ) ) {
+        switch( (int)Game.cookies["showersOn"] ) {
+          case 0:
+            Game.script.ShowSpeechBubble( "Hey! I can clearly see you tryin' to steal my radio!", transform.Find( "BubTarget" ), 3f );
+            break;
+          case 1:
+            Game.script.ShowSpeechBubble( "Hey! I see you tryin' to steal my radio!", transform.Find( "BubTarget" ), 3f );
+            break;
+          case 2:
+            Game.script.ShowSpeechBubble( "Hey! I can kinda see you tryin' to steal my radio!", transform.Find( "BubTarget" ), 3f );
+            break;
+          case 3:
+            Game.script.ShowSpeechBubble( "Hey! I can barely see you tryin' to steal my radio!", transform.Find( "BubTarget" ), 3f );
+            break;
+          case 4:
+            Game.script.AddItem( "radio" );
+            Destroy( gameObject );
+            break;
+          default:
+            Game.script.ShowSpeechBubble( "Hey! I can clearly see you tryin' to steal my radio!", transform.Find( "BubTarget" ), 3f );
+            break;
+        }
+      } else {
+        Game.script.ShowSpeechBubble( "Hey! I can clearly see you tryin' to steal my radio!", transform.Find( "BubTarget" ), 3f );
       }
     } );
   }
