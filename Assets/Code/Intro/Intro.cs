b77d4ad9d4403f040e1e0515ba94acfa4009f87d
oldfashionedrobot <sanjeev@oldfashionedrobot.com>
2014-09-14 15:56:00 -0400

    guard escort for intro

--- Assets/Code/Intro/Intro.cs
+++ Assets/Code/Intro/Intro.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class Intro : MonoBehaviour {
 
   private GameObject player;
+  private GameObject guard;
   
   private bool canDo = false;
+  private bool moveGuard = false;
+
 
   void Start() {
     player = GameObject.Find( "Player" );
+    guard = GameObject.Find( "Guard" );
     player.GetComponent<Player>().MoveTo( GameObject.Find( "InCellTarget" ).transform.position );
     player.GetComponent<Player>().PauseNav();
     StartCoroutine( WaitForEnter() );
@@ -23,7 +27,24 @@ public class Intro : MonoBehaviour {
   void Update() {
     if( player.transform.localPosition.y > -0.3164263f ) {
       player.GetComponent<LayerSetter>().SetOrder( 0 );
-    }    
+    }  
+    
+    if( guard && guard.transform.position.x > -0.739535f && guard.transform.parent.gameObject.name == "Player" ) {
+      guard.transform.parent = guard.transform.parent.parent;
+      ShowSpeechBubble( "Get comfy maggot.\nYou're gonna be in there a while.", guard.transform.Find( "BubTarget" ), 4f );
+    }  
+    
+    if( moveGuard ) {
+      guard.transform.position = new Vector3( guard.transform.position.x - 0.01f, guard.transform.position.y, guard.transform.position.z );
+    }
+  }
+  
+  public void ShowSpeechBubble( string text, Transform target, float time ) {
+    Destroy( GameObject.FindWithTag( "SpeechBubble" ) );
+    GameObject bub = Instantiate( Resources.Load( "SpeechBubble" ) ) as GameObject;
+    bub.GetComponent<SpeechBubble>().text = text;
+    bub.GetComponent<SpeechBubble>().target = target;
+    bub.GetComponent<SpeechBubble>().time = time;
   }
 
   private IEnumerator WaitForEnter() {
@@ -37,7 +58,11 @@ public class Intro : MonoBehaviour {
 
     player.transform.localScale = new Vector3( -player.transform.localScale.x, player.transform.localScale.y, player.transform.localScale.z );
     
-    yield return new WaitForSeconds( 0f );
+    yield return new WaitForSeconds( 1f );
+    
+    moveGuard = true;
+    
+    yield return new WaitForSeconds( 3f );
     
     // Zooming and Fading stuff
     GameObject sprites = GameObject.Find( "Sprites" );
@@ -65,6 +90,8 @@ public class Intro : MonoBehaviour {
     
     PolyNav2D.current.GenerateMap();
     
+    moveGuard = false;
+    Destroy( guard );
     
     player.GetComponent<Player>().ResumeNav();
     player.GetComponent<PolyNavAgent>().maxSpeed = 1f;
