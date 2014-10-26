56148ff61e910ed19c9bb16b4720a5b7afd2dee1
oldfashionedrobot <sanjeev@oldfashionedrobot.com>
2014-10-26 16:46:58 -0400

    removed old stuff

--- OLDSTUFF/Code/CamScripts/ZoomView.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class ZoomView : MonoBehaviour {
-  
-	//Panning / parallax should go here.
-}
--- OLDSTUFF/Code/ItemScripts/ItemCombo.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-// just used for the inspector
-
-[System.Serializable]
-public class ItemCombo {
-  public GameObject combinesWith;
-  public GameObject turnsInto;
-}
